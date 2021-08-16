78890b5d6abb537fca9f6680768a9cd8f8db1465
Diaz Amaliana <diazamaliana@gmail.com>
2021-08-17 01:27:10 +0700

    add pages

---
+++ src/pages/CreatePlaylist/index.jsx
@@ -0,0 +1,7 @@
+const CreatePlaylist = () => {
+    return (
+        <p>Ini page Create Playlist</p>
+    )
+}
+
+export default CreatePlaylist
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <p>Ini page Home</p>
+    )
+}
+
+export default Home
