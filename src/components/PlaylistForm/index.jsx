6c31999815fa05a344e5d8f99738423f51bb1b09
Diaz Amaliana <diazamaliana@gmail.com>
2021-08-17 19:15:23 +0700

    add PlaylistForm component

---
+++ src/components/PlaylistForm/index.jsx
@@ -0,0 +1,98 @@
+import { useToast, Box, Heading, Input, Textarea, Button } from '@chakra-ui/react'
+import { useDispatch, useSelector } from "react-redux"
+import { clearSelectedTracks } from '../../redux/store/playlist'
+import { setForm } from "../../redux/store/playlist"
+import { createPlaylist, addTrackToPlaylist } from "../../libraries/spotify"
+
+const PlaylistForm = () => {
+  const { accessToken, user } = useSelector(state => state.auth)
+  const { selectedTracks, form } = useSelector(state => state.playlist)
+  const toast = useToast()
+
+  const dispatch = useDispatch()
+
+  const handleFormChanges = e => {
+      dispatch(setForm({[e.target.name]: e.target.value}))
+  }
+
+  const handleSubmit = e => {
+       e.preventDefault()
+       if (selectedTracks.length > 0){
+           createPlaylist(accessToken, user.id, {
+           name: form.title,
+           description: form.description,
+           public: false,
+           collaborative: false
+         }).then(playlist => {
+           return addTrackToPlaylist(accessToken, playlist.id, {
+             uris: selectedTracks
+           })
+         }).then(() => {
+            dispatch(clearSelectedTracks())
+            toast({
+                title: "Playlist created.",
+                position: "top",
+                description: "We've successfully created a playlist for you.",
+                status: "success",
+                duration: 9000,
+                isClosable: true,
+            })
+         })
+       } else {
+         toast({
+            title: "Information",
+            position: "top",
+            description: "Please selected some tracks to make a playlist.",
+            status: "info",
+            duration: 9000,
+            isClosable: true,
+          })
+       }    
+   }
+
+  return (
+    <Box 
+        marginBottom="1.5rem"
+        textAlign="left"
+        padding=".75rem">
+        <Heading>Create Playlist</Heading>
+        <form style={{ marginTop:"0.5em" }} onSubmit={handleSubmit}>
+            <label htmlFor="title">Title</label>
+            <Input
+                placeholder="Playlist Title"
+                marginTop="0.5em"
+                padding="1em"
+                borderRadius="0.5rem"
+                bgColor={'dark.700'} 
+                type="text"
+                name="title"
+                minLength="10"
+                onChange={handleFormChanges}
+                value={form.title}
+            />
+            <label htmlFor="description">Description</label>
+            <Textarea
+                placeholder="Playlist Description"
+                padding="1em"
+                marginTop="0.5em"
+                borderRadius="0.5rem"
+                bgColor={'dark.700'} 
+                name="description"
+                minLength="10"
+                onChange={handleFormChanges}
+                value={form.description}
+            ></Textarea>
+            <Button
+                type="submit"
+                marginTop="0.5em"
+                px='1.5em'
+                bg={'brand.300'}
+                color={'white'}
+                _hover={{ bg: 'whiteAlpha.500' }}
+            > Create Playlist </Button>
+        </form>
+    </Box>
+
+  )
+}
+export default PlaylistForm
