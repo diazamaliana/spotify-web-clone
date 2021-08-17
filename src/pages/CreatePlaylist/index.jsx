8828bb76e3743b83ab5f1bd600edead79612cbb8
Diaz Amaliana <diazamaliana@gmail.com>
2021-08-18 00:50:20 +0700

    edit create-playlist pages

--- src/pages/CreatePlaylist/index.jsx
+++ src/pages/CreatePlaylist/index.jsx
@@ -1,7 +1,57 @@
-const CreatePlaylist = () => {
+import { useState } from "react";
+import { Box, Grid } from '@chakra-ui/react'
+import PlaylistForm from "../../components/PlaylistForm"
+import Track from '../../components/Track'
+import SearchBar from '../../components/SearchBar'
+import { getSearchTrack  } from "../../libraries/spotify";
+import { setTracks } from "../../redux/store/playlist";
+import { useSelector, useDispatch } from 'react-redux';
+
+const CreatePlaylist= () => {
+  const { accessToken } = useSelector(state => state.auth)
+  const { tracks } = useSelector(state => state.playlist)
+  const dispatch = useDispatch()
+  
+  const [isLoading, setIsloading] = useState(false)
+
+  const handleSearch = (query) => {
+      const options = {
+        q: query,
+        type: 'track',
+        limit: 15,
+      };
+      setIsloading(true);
+      getSearchTrack(accessToken, options).then((res) => {
+        dispatch(setTracks(res.tracks.items));
+        setIsloading(false);
+      });
+  };
+    
     return (
-        <p>Ini page Create Playlist</p>
-    )
+    <Box >
+        <PlaylistForm />
+        <SearchBar handleSearch = {handleSearch} />
+        
+        {isLoading ? (
+            <p>Loading...</p>
+        ) : (
+          <Grid
+            width={'auto'}
+            padding="0.75rem"
+            templateRows='auto'
+            templateColumns="repeat(1, 1fr)"
+            gap={2}
+            marginTop=".5rem">
+            {tracks.map(track => (
+            <Track  track={track} key={track.id} />
+            ))}   
+        </Grid>
+        )}  
+
+    </Box>
+    ) 
 }
 
-export default CreatePlaylist
+export default CreatePlaylist;
+
+        
